c5d860fdc1e726cc23857fc9cf0cede753dea1bb
Marina Russ <mrusscode@gmail.com>
2024-12-10 16:28:38 -0500

    imports GuessInput component for rendering

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import GuessInput from "../GuessInput";
+
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
-// Pick a random word on every pageload.
+// Pick a random word on every page load.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return (
+    <GuessInput />
+  );
 }
 
 export default Game;
