3d1a9d77445e1444a07a97a244e5a5c444fb9302
Marina Russ <mrusscode@gmail.com>
2024-12-13 12:26:01 -0500

    renders GuessResults & GuessInput components

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 import GuessInput from "../GuessInput";
+import GuessResults from "../GuessResults";
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 
 // Pick a random word on every page load.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
+// For debugging:
 console.info({ answer });
 
 function Game() {
+
+  const [guessHistory, setGuessHistory] = React.useState([]);
+
   return (
-    <GuessInput />
+    <>
+      <GuessResults
+        guessHistory={guessHistory}
+      />
+      <GuessInput
+        guessHistory={guessHistory}
+        setGuessHistory={setGuessHistory}
+      />
+
+    </>
   );
 }
 
